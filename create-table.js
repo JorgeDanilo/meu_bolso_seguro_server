0104f24834ae678b792a86258a7de1e28ee48e34
Jorge Danilo <dev03@access.run>
2019-11-18 17:05:35 -0300

    implementacoes

--- create-table.js
+++ create-table.js
@@ -20,31 +20,42 @@
 //GRANT ALL PRIVILEGES ON meu_bolso_seguro.* TO 'admin'@'%' WITH GRANT OPTION;
 //FLUSH PRIVILEGES;
 
-const Connection = require('../meu_bolso_seguro_server/config/db');
-const mysql = Connection();
-mysql.open();
+// const Connection = require('../meu_bolso_seguro_server/config/db');
+// const mysql = Connection();
+// mysql.open();
 
-const sql = "CREATE TABLE IF NOT EXISTS Clientes (\n"+
-                  "ID int NOT NULL AUTO_INCREMENT,\n"+
-                  "Nome varchar(150) NOT NULL,\n"+
-                  "CPF char(11) NOT NULL,\n"+
-                  "PRIMARY KEY (ID)\n"+
-                  ");";
+// const sql = "CREATE TABLE IF NOT EXISTS Clientes (\n"+
+//                   "ID int NOT NULL AUTO_INCREMENT,\n"+
+//                   "Nome varchar(150) NOT NULL,\n"+
+//                   "CPF char(11) NOT NULL,\n"+
+//                   "PRIMARY KEY (ID)\n"+
+//                   ");";
 
-mysql.getConnection().query(sql, function(error, results, fields) {
-    if(error) return console.log(error);
-    console.log('create table');
-});
+// mysql.getConnection().query(sql, function(error, results, fields) {
+//     if(error) return console.log(error);
+//     console.log('create table');
+// });
 
 
-const sqlInsert = "INSERT INTO Clientes(Nome, CPF) VALUES ?";
-const values = [
-    ['teste1', '12345678901'],
-    ['teste1', '09876543210'],
-    ['teste3', '12312312399']
-];
+// const sqlInsert = "INSERT INTO Clientes(Nome, CPF) VALUES ?";
+// const values = [
+//     ['teste1', '12345678901'],
+//     ['teste1', '09876543210'],
+//     ['teste3', '12312312399']
+// ];
 
-mysql.getConnection().query(sqlInsert, [values], function(error, results, fields) {
-    if(error) console.log(error);
-    console.log('create insert!');
-}) ;
+// mysql.getConnection().query(sqlInsert, [values], function(error, results, fields) {
+//     if(error) console.log(error);
+//     console.log('create insert!');
+// }) ;
+
+
+// const sqlSelect = "select * from Clientes";
+// mysql.getConnection().query(sqlSelect, function(error, results, fields) {
+//     if(error) console.log(error);
+//     console.log(results);
+// });
+
+var user = require('../meu_bolso_seguro_server/models/user')
+user.setName('Teste');
+user.sayHello();
---
+++ models/user.js
@@ -0,0 +1,26 @@
+function User() {
+}
+
+User.prototype.setName = function(name) {
+    this.name = name;
+};
+
+User.prototype.setPassword = function(password) {
+    this.password = password;
+};
+
+User.prototype.getName = function() {
+    return this.name;
+};
+
+User.prototype.getPassword = function() {
+    return this.password;
+};
+
+User.prototype.getUser = function() {
+    return this.name, this.password
+}
+
+var user = new User();
+
+module.exports = user;
