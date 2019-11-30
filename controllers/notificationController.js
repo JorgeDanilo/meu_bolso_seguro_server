9547cf66f05563cfbcfdb0f4644b6ee7267fc523
Jorge Danilo <sobrejavatec@gmail.com>
2019-11-30 15:54:40 -0300

    testing push notification with api dispatcher firebase

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -4,28 +4,55 @@ const repository = require('../repositories/notificationRepository');
 const serviceAccount = require('../config/meu-bolso-seguro.json');
 
 exports.send = (req, res, next) => {
-    var userId = req.param.userId;
-    repository.findById(userId).then(notification => {
-        const firebaseToken = notification.token;
-        firebase.initializeApp({
-            credential: firebase.credential.cert(serviceAccount),
-            databaseURL: 'https://meu-bolso-seguro.firebaseio.com'
-        });
+    var firebaseToken = "dI-bHNCja-8:APA91bEt4MVD4WjyUe7Ce-s4NVDH9j624oR4LKyP8NjIo7WjU3DffHOJL6pcs3UafO0SebWHvfVLYuluOtOjvRSCu2vyDrr96gxPUD6FzaxznB4Bl1U5Nu0wcWO_J1CqA2dPAlxA19YC"
+    // var userId = req.param.userId;
+    // repository.findById(userId).then(notification => {
+    //     const firebaseToken = notification.token;
+    //     firebase.initializeApp({
+    //         credential: firebase.credential.cert(serviceAccount),
+    //         databaseURL: 'https://meu-bolso-seguro.firebaseio.com'
+    //     });
         
-        const payload = {
-            notification: {
-                title: 'Notification title',
-                body: 'This is an example notification',
-            }
-        };
+    //     const payload = {
+    //         notification: {
+    //             title: 'Notification title',
+    //             body: 'This is an example notification',
+    //         }
+    //     };
         
-        const options = {
-            priority: 'high',
-            timeToLive: 60 * 60 * 24,   
-        };
+    //     const options = {
+    //         priority: 'high',
+    //         timeToLive: 60 * 60 * 24,   
+    //     };
         
-        firebase.messaging.sendToDevice(firebaseToken, payload, options);
-    })
+    //     firebase.messaging.sendToDevice(firebaseToken, payload, options);
+    // })
+
+
+
+    firebase.initializeApp({
+        credential: firebase.credential.cert(serviceAccount),
+        databaseURL: 'https://meu-bolso-seguro.firebaseio.com'
+    });
+    
+    const payload = {
+        notification: {
+            title: 'Notification title Via Api',
+            body: 'This is an example notification',
+        }
+    };
+    
+    const options = {
+        priority: 'high',
+        timeToLive: 60 * 60 * 24,   
+    };
+    
+    
+    firebase.messaging().sendToDevice(firebaseToken, payload, options).then(result => {
+        res.status(200).send(res);
+    }).catch(err => {
+        res.status(503).send(err);
+    });
    
 };
 
--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/notificationController');
 router.post('/', controller.register);
+router.get('/send', controller.send);
+
 module.exports = router;
